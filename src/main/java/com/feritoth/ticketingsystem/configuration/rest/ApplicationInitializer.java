abbe5776f0ae4482cab6134ec24ce5614404be25
Toth <ferenc.toth@cgi.com>
2019-10-01 09:47:59 +0200

    Minor adjustments in code and configuration: configuration comments and cleanup

--- src/main/java/com/feritoth/ticketingsystem/configuration/rest/ApplicationInitializer.java
+++ src/main/java/com/feritoth/ticketingsystem/configuration/rest/ApplicationInitializer.java
@@ -9,6 +9,7 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
+/* This annotation together with its parameter will ensure that the current initializer will be executed before the setup of the Jersey context */
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ApplicationInitializer implements WebApplicationInitializer {
 
@@ -16,7 +17,10 @@ public class ApplicationInitializer implements WebApplicationInitializer {
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		/* Create the annotation configuration context and use it for configuring the ServletContext passed in as parameter */
 		AnnotationConfigWebApplicationContext configContext = new AnnotationConfigWebApplicationContext();
+		/* Add the context listener for enabling the load procedure */
 		servletContext.addListener(new ContextLoaderListener(configContext));
+		/* Add the setup location - the package to be scanned for Jersey references (this will prevent Jersey from searching for the
+		   Spring configuration file applicationContext.xml and throw a FileNotFoundException) */
 		servletContext.setInitParameter("contextConfigLocation", "com.feritoth.ticketingsystem");
 	}
 
